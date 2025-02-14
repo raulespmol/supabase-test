5dc7518bb6990537c7f800a3482a639faef6f3b4
Raul Espinoza M <raul.espmol@gmail.com>
2025-02-14 16:22:49 -0300

    [fix] anon key const name

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,7 +1,7 @@
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
-const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export {
   SUPABASE_URL,
-  SUPABASE_KEY
+  SUPABASE_ANON_KEY
 }
