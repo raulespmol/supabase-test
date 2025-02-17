139bea75d0083bf6293ccdb95c5d19bc864b345e
Raul Espinoza M <raul.espmol@gmail.com>
2025-02-17 18:13:47 -0300

    add: formulario basico
    permite agregar items correctamente

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,10 +1,71 @@
-import React from 'react'
+import { Button, TextField } from '@mui/material'
+import React, { useState, useContext } from 'react'
+import { AppContext } from '../context/AppContext'
 
 const Formulario = () => {
+  const { createItem } = useContext(AppContext)
+  const [nuevoItem, setNuevoItem] = useState({})
+
+  const handleItem = e => {
+    setNuevoItem({
+      ...nuevoItem,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    await createItem(nuevoItem)
+    setNuevoItem({})
+  }
+
   return (
-    <div className="mt-4 text-white">
-      Formulario
-    </div>
+    <form 
+      className="mt-4"
+      onSubmit={handleSubmit}
+    >
+      <h3>Agregar Item</h3>
+      <div className="flex flex-col">
+        <TextField 
+          name="nombre" 
+          label="Nombre" 
+          variant="filled"
+          onChange={handleItem}
+        />
+        <TextField 
+          name="material" 
+          label="Material" 
+          variant="filled"
+          onChange={handleItem}
+        />
+        <TextField 
+          name="medidas" 
+          label="Medidas" 
+          variant="filled"
+          onChange={handleItem}
+        />
+        <TextField 
+          name="cantidad" 
+          label="Cantidad" 
+          variant="filled"
+          onChange={handleItem}
+        />
+        <TextField 
+          name="observaciones" 
+          label="Observaciones" 
+          variant="filled"
+          onChange={handleItem}
+        />
+        <Button 
+          variant="contained" 
+          color="primary"
+          type='submit'
+        >
+          Agregar
+        </Button>
+        
+      </div>
+    </form>
   )
 }
 
