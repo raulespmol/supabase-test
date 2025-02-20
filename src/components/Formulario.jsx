c0de4fc9f1bf56d032360c1b0c7c92fe5c59bb8c
Raul Espinoza M <raul.espmol@gmail.com>
2025-02-20 19:59:20 -0300

    update: useForm
    probar hooks register y handleSubmit

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,57 +1,65 @@
 import { Button, FormControl, InputLabel, MenuItem, Select, TextField } from '@mui/material'
 import React, { useState, useContext } from 'react'
+import { useForm } from 'react-hook-form'
 import { AppContext } from '../context/AppContext'
 import { campos } from '../data/constants'
 
-const nuevoItemInicial = () => {
-  return campos.reduce((acc, campo) => {
-    acc[campo.toLowerCase()] = ''
-    return acc
-  }, {})
-}
+// const nuevoItemInicial = () => {
+//   return campos.reduce((acc, campo) => {
+//     acc[campo.toLowerCase()] = ''
+//     return acc
+//   }, {})
+// }
 
 const Formulario = () => {
   const { createItem, materials } = useContext(AppContext)
-  const [nuevoItem, setNuevoItem] = useState(nuevoItemInicial)
+  // const [nuevoItem, setNuevoItem] = useState(nuevoItemInicial)
   const [isSubmitting, setIsSubmitting] = useState(false)
+  const {register, handleSubmit } = useForm()
 
-  const handleItem = e => {
-    setNuevoItem({
-      ...nuevoItem,
-      [e.target.name]: e.target.value
-    })
-  }
+  const onSubmit = handleSubmit((data) => {
+    console.log(data)
+  })
 
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-    setIsSubmitting(true)
-    try {
-      await createItem(nuevoItem)
-      setNuevoItem(nuevoItemInicial)
-    } catch (error) {
-      console.error("Error al crear el item", error)
-    } finally {
-      setIsSubmitting(false)
-    }
-  }
+  // const handleItem = e => {
+  //   setNuevoItem({
+  //     ...nuevoItem,
+  //     [e.target.name]: e.target.value
+  //   })
+  //   console.log(nuevoItem)
+  // }
+
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault()
+  //   setIsSubmitting(true)
+  //   try {
+  //     await createItem(nuevoItem)
+  //     setNuevoItem(nuevoItemInicial)
+  //   } catch (error) {
+  //     console.error("Error al crear el item", error)
+  //   } finally {
+  //     setIsSubmitting(false)
+  //   }
+  // }
 
   return (
     <form 
       className="mt-4"
-      onSubmit={handleSubmit}
+      onSubmit={onSubmit}
     >
       <h3>Agregar Item</h3>
       <div className="flex flex-col gap-3">
         {campos.map(campo => (
-          campo.toLowerCase() === 'material' ? (
+          campo.toLowerCase() === 'material' ? ( 
             <FormControl fullWidth key={campo}>
-              <InputLabel id="material-label">Material</InputLabel>
+              <InputLabel id={campo + "-label"}>Material</InputLabel>
               <Select
-                labelId="material-label"
-                value={nuevoItem.material}
-                name="material"
-                label="Material"
-                onChange={handleItem}
+                labelId={campo + "-label"}
+                label={campo}
+                {...register(campo.toLowerCase())}
+                // value={nuevoItem.material}
+                // name="material"
+                // onChange={handleItem}
               >
                 {materials.map(material => (
                   <MenuItem key={material.id} value={material.id}>{material.nombre}</MenuItem>
@@ -61,11 +69,12 @@ const Formulario = () => {
             ) : (
             <TextField 
               key={campo}
-              name={campo.toLowerCase()} 
               label={campo} 
               variant="outlined"
-              value={nuevoItem[campo.toLowerCase()]}
-              onChange={handleItem}
+              {...register(campo.toLowerCase())}
+              // name={campo.toLowerCase()} 
+              // value={nuevoItem[campo.toLowerCase()]}
+              // onChange={handleItem}
             />
             )
           ))
