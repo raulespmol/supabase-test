9f4bec8708233fb0c7f6608c08976e3eca538251
Raul Espinoza M <raul.espmol@gmail.com>
2025-02-22 14:02:36 -0300

    update: integrar react-hook-form
    inputs controlados, pendiente validaciones

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,97 +1,94 @@
-import { Button, FormControl, InputLabel, MenuItem, Select, TextField } from '@mui/material'
-import React, { useState, useContext } from 'react'
-import { useForm } from 'react-hook-form'
-import { AppContext } from '../context/AppContext'
-import { campos } from '../data/constants'
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
+import React, { useState, useContext } from "react";
+import { useForm } from "react-hook-form";
+import { AppContext } from "../context/AppContext";
+import { campos } from "../data/constants";
 
-// const nuevoItemInicial = () => {
-//   return campos.reduce((acc, campo) => {
-//     acc[campo.toLowerCase()] = ''
-//     return acc
-//   }, {})
-// }
+const nuevoItemInicial = {
+  nombre: "",
+  material: "",
+  medidas: "",
+  cantidad: "",
+  observaciones: "",
+}
 
 const Formulario = () => {
-  const { createItem, materials } = useContext(AppContext)
-  // const [nuevoItem, setNuevoItem] = useState(nuevoItemInicial)
-  const [isSubmitting, setIsSubmitting] = useState(false)
-  const {register, handleSubmit } = useForm()
+  const { createItem, materials } = useContext(AppContext);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const { register, handleSubmit, watch } = useForm({
+    defaultValues: nuevoItemInicial
+  });
 
   const onSubmit = handleSubmit((data) => {
-    console.log(data)
-  })
-
-  // const handleItem = e => {
-  //   setNuevoItem({
-  //     ...nuevoItem,
-  //     [e.target.name]: e.target.value
-  //   })
-  //   console.log(nuevoItem)
-  // }
-
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault()
-  //   setIsSubmitting(true)
-  //   try {
-  //     await createItem(nuevoItem)
-  //     setNuevoItem(nuevoItemInicial)
-  //   } catch (error) {
-  //     console.error("Error al crear el item", error)
-  //   } finally {
-  //     setIsSubmitting(false)
-  //   }
-  // }
+    console.log(data);
+  });
 
   return (
-    <form 
-      className="mt-4"
-      onSubmit={onSubmit}
-    >
+    <form className="mt-4" onSubmit={onSubmit}>
       <h3>Agregar Item</h3>
       <div className="flex flex-col gap-3">
-        {campos.map(campo => (
-          campo.toLowerCase() === 'material' ? ( 
-            <FormControl fullWidth key={campo}>
-              <InputLabel id={campo + "-label"}>Material</InputLabel>
-              <Select
-                labelId={campo + "-label"}
-                label={campo}
-                {...register(campo.toLowerCase())}
-                // value={nuevoItem.material}
-                // name="material"
-                // onChange={handleItem}
-              >
-                {materials.map(material => (
-                  <MenuItem key={material.id} value={material.id}>{material.nombre}</MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-            ) : (
-            <TextField 
-              key={campo}
-              label={campo} 
-              variant="outlined"
-              {...register(campo.toLowerCase())}
-              // name={campo.toLowerCase()} 
-              // value={nuevoItem[campo.toLowerCase()]}
-              // onChange={handleItem}
-            />
-            )
-          ))
-        }
-        
-        <Button 
-          variant="contained" 
+        <TextField
+          name="nombre"
+          label="Nombre"
+          variant="outlined"
+          {...register("nombre")}
+        />
+
+        <FormControl fullWidth>
+          <InputLabel id="material-label">Material</InputLabel>
+          <Select
+            labelId="material-label"
+            label="Material"
+            value={watch("material")}
+            {...register("material")}
+          >
+            {materials.map((material) => (
+              <MenuItem key={material.id} value={material.id}>
+                {material.nombre}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+
+        <TextField
+          name="medidas"
+          label="Medidas"
+          variant="outlined"
+          {...register("medidas")}
+        />
+
+        <TextField
+          name="cantidad"
+          label="Cantidad"
+          variant="outlined"
+          {...register("cantidad")}
+        />
+
+        <TextField
+          name="observaciones"
+          label="Observaciones"
+          variant="outlined"
+          {...register("observaciones")}
+        />
+
+        <Button
+          variant="contained"
           color="primary"
           type="submit"
           loading={isSubmitting}
         >
           Agregar
         </Button>
-        
       </div>
     </form>
-  )
-}
+  );
+};
 
-export default Formulario
+export default Formulario;
